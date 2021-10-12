b68d8f634d7956efb7a76605a07d6bd59896b9e3
harsh-9in <harshkumar9.in@gmail.com>
2021-10-12 22:10:58 +0530

    routers added

---
+++ src/containers/CreateContact/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateContainer = () => {
+    return (
+        <div>
+            <h1>CreateContact</h1>
+        </div>
+    )
+}
+
+export default CreateContainer;
