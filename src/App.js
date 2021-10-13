51c3ade066efe63a4cb53c8173e76d79d5df6a84
Harsh Kumar <54139847+harsh-9in@users.noreply.github.com>
2021-10-13 12:46:36 +0530

    Login and Register page created (#2)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router,Switch,Route } from 'react-router-dom';
+import routes from './routes';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <Router>
+        <Switch>
+          {routes.map((route,index)=>(
+          <Route key={index} path={route.path} exact render={(props)=><route.component {...props}/>}></Route>
+          ))}
+        </Switch>
+      </Router>
     </div>
   );
 }
