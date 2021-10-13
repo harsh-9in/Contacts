bbcd5c38751ed896a9abe172557167f9b80c60f4
Harsh Kumar <54139847+harsh-9in@users.noreply.github.com>
2021-10-13 17:32:48 +0530

    Harsh branch (#4)

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Menu } from 'semantic-ui-react';
+import { Menu,Image,Button, Icon } from 'semantic-ui-react';
+import logo from '../../assects/images/logo.svg';
 
 const Header = () => {
     return (
-        <Menu secondary>
-            <Menu.Item>TrulyContacts</Menu.Item>
-            <Menu.Item>Create Contacts</Menu.Item>
-            <Menu.Item>Logout</Menu.Item>
+        <Menu secondary pointing>
+            <Image src={ logo } width={60}/>
+            <Menu.Item style={{ fontSize:24 }}>TrulyContacts</Menu.Item>
+            <Menu.Item position='right'>Create Contacts</Menu.Item>
+            <Menu.Item >Logout</Menu.Item>
+
+            <Button icon>
+                <Icon name='add'></Icon>
+                Create Contact
+            </Button>
         </Menu>
     );
 };
